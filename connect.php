6804887fd2a741eaf09dfb6e6ee7e770da5fcf08
Sai Praveen <ravikumar_csd@srkrec.edu.in>
2023-08-25 16:21:48 +0530

    codemaster temp

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+
+$conn=mysqli_connect('localhost','codemaster2018','codemaster2018@6464', 'codemaster2018');
+$conn2=mysqli_connect('localhost','codemaster2018','codemaster2018@6464', 'moodle');
+
+?>
